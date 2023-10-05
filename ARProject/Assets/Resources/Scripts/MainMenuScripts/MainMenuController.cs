9f2ec69744813302c842d4b4f34e58a40d1614fa
GustavoMendozaAren <stavmdza@gmail.com>
2023-10-04 21:01:51 -0600

    MainMenu

---
+++ ARProject/Assets/Resources/Scripts/MainMenuScripts/MainMenuController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private string url;
+    [SerializeField] private string url2;
+
+    public void OpenInstagram()
+    {
+        Application.OpenURL(url);
+    }
+
+    public void OpenPagina()
+    {
+        Application.OpenURL(url2);
+    }
+    public void ScanButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
